8f7607155f939929f319a8f34efd444c8dd50629
Tense11z <tensei.60@gmail.com>
2024-06-19 01:16:22 +0300

    added wincondition function

--- script.js
+++ script.js
@@ -9,6 +9,20 @@ let playerZeroInput = '';
 let playerOneInput = '';
 let playerZeroArr = new Array;
 let playerOneArr = new Array;
+const winConditions = [
+    // Rows
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    // Columns
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    // Diagonals
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
 
 // function to clear board
 function clearBoard() {
@@ -23,7 +37,7 @@ function playerOrder() {
     if (currentPlayer === playerZero) {
         console.log(`playerZero starts`)
         playerZeroInput = 'x';
-        playerOneInput = 'o';
+        playerOneInput = 'o'; //○
     } else {
         console.log(`playerOne starts`);
         playerZeroInput = 'o';
@@ -54,12 +68,21 @@ function removeFirst() {
     }
 }
 
+// function to check wincondition
 function checkWinCondition() {
     if (playerOneArr.length == 3 || playerZeroArr.length == 3) {
         if (currentPlayer === 0) {
-
+            for (let i = 0; i < winConditions.length; i += 1) {
+                if (playerZeroArr.every(elem => winConditions[i].includes(elem))) {
+                    return console.log(`${currentPlayer} has won`)
+                }
+            }
         } else {
-
+            for (let i = 0; i < winConditions.length; i += 1) {
+                if (playerOneArr.every(elem => winConditions[i].includes(elem))) {
+                    return console.log(`${currentPlayer} has won`)
+                }
+            }
         }
     }
 }
@@ -73,11 +96,13 @@ Array.from(boardElement.children).forEach((cell, index) => {
                 clickedCell.textContent = playerZeroInput;
                 playerZeroArr.push(index);
                 removeFirst()
+                checkWinCondition()
                 switchPlayer();
             } else {
                 clickedCell.textContent = playerOneInput;
                 playerOneArr.push(index);
                 removeFirst()
+                checkWinCondition()
                 switchPlayer();
             }
         }
